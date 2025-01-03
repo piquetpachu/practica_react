6ca6a6f4755062afdac603999b1ea6240936a3cd
piquetpachu <pachu510121@gmail.com>
2025-01-03 16:38:52 -0300

    tercer commit

--- 02-ta-te-ti/src/App.jsx
+++ 02-ta-te-ti/src/App.jsx
@@ -6,6 +6,17 @@ const Turn = {
   o:'O'
 }
 
+const combosGanadores = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 const Cuadrado = ({children,isSelected,actualizaTablero,index}) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
   const hacerclick = () => {
@@ -16,19 +27,55 @@ const Cuadrado = ({children,isSelected,actualizaTablero,index}) => {
   )  
 }
 
+
+
 function App ()  {
   const [tablero,setTablero] = useState(Array(9).fill(null))
   console.log(tablero)
 
   const [turn,setTurn] = useState(Turn.x) 
 
+  const [ganador,setGanador] = useState(null)
+
+  const checkJuego = (nuevoTablero) => {
+    return nuevoTablero.every(Cuadrado => Cuadrado !== null)
+  }
+
+  const hayGanador = (checkTablero) => {
+    for (const combos of combosGanadores) {
+      const [a,b,c] = combos
+      if (checkTablero[a] && checkTablero[a] === checkTablero[b] && checkTablero[a] === checkTablero[c]){
+        return checkTablero[a]
+      }
+    }
+    return null 
+  }
+  const reset = () =>{
+    setTablero(Array(9).fill(null))
+    setTurn(Turn.x)
+    setGanador(null)
+  }
+    
   const actualizaTablero = (index) =>{
+    if(tablero[index] || ganador) return 
+
     const nuevoTablero = [...tablero]
     nuevoTablero[index] = turn
     setTablero(nuevoTablero)
+
     const nuevoTurno = turn === Turn.x ? Turn.o : Turn.x
     setTurn(nuevoTurno)
+    
+    const elGanador = hayGanador(nuevoTablero)
+    if (elGanador){
+      setGanador(elGanador)
+    } else if(checkJuego(nuevoTablero)){
+      setGanador(false)
+    }
+
+
   }
+
   return (
     <main className=" board">
       <h1>Ta-Te-Ti</h1>
@@ -47,6 +94,20 @@ function App ()  {
         <Cuadrado isSelected={turn === Turn.x}>{Turn.x}</Cuadrado>
         <Cuadrado isSelected={turn === Turn.o}>{Turn.o}</Cuadrado>
       </section>
+      {ganador !== null && (
+      <section className="winner">
+        <div className="text">
+          <h2>{ganador === false ? 'Empate':'ganador '}</h2>
+          <header>
+            {ganador && <Cuadrado>{ganador}</Cuadrado>}
+          </header>
+          <footer>
+            <button onClick={reset}>empezar de nuevo</button>
+          </footer>
+        </div>
+      </section>
+      )
+      }
     </main>
   )
 }
